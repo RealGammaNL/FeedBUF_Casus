746cef064b5cbcb3e18e3ee76d335e658513c9ef
Vigo <2101466luthjens@zuyd.nl>
2023-03-31 10:45:51 +0200

    Save 1

--- Forms/TeacherForm.Designer.cs
+++ Forms/TeacherForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FeedBUF
+﻿namespace FeedBUF_Casus.Forms
 {
     partial class TeacherForm
     {
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TeacherForm));
+            this.MainGroup = new System.Windows.Forms.GroupBox();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.MainGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainGroup
+            // 
+            this.MainGroup.BackColor = System.Drawing.Color.Orange;
+            this.MainGroup.Controls.Add(this.HomeButton);
+            this.MainGroup.Location = new System.Drawing.Point(-3, -11);
+            this.MainGroup.Name = "MainGroup";
+            this.MainGroup.Size = new System.Drawing.Size(1904, 78);
+            this.MainGroup.TabIndex = 0;
+            this.MainGroup.TabStop = false;
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.BackColor = System.Drawing.Color.Orange;
+            this.HomeButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("HomeButton.BackgroundImage")));
+            this.HomeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.HomeButton.Location = new System.Drawing.Point(6, 22);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(51, 50);
+            this.HomeButton.TabIndex = 1;
+            this.HomeButton.UseVisualStyleBackColor = false;
+            // 
+            // TeacherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DocentForm";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1896, 1016);
+            this.Controls.Add(this.MainGroup);
+            this.Name = "TeacherForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TeacherForm";
+            this.MainGroup.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox MainGroup;
+        private System.Windows.Forms.Button HomeButton;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FeedBUF_Casus.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace FeedBUF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new TeacherForm());
         }
     }
 }
