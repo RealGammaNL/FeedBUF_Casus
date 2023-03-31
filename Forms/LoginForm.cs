40f4768ef8aad9d2a1d8c2755a5fb48a5578f6d6
Gamma <max@brosky.net>
2023-03-31 10:16:10 +0200

    retry

--- Forms/LoginForm.cs
+++ Forms/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DOMAIN;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,56 @@ namespace FeedBUF
 {
     public partial class LoginForm : Form
     {
+
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        public void SignInButton_Click(object sender, EventArgs e)
+        {
+            bool found = false;
+            foreach (User user in Users)
+            {
+                if (user.Email == LoginEmail_Box.Text && user.Password == LoginPassword_Box.Text && user.SecurityLevel == "Manager")
+                {
+                    LoginPanel.Hide();
+                    ManageAcc_Btn.Show();
+                    CreateAccBtn.Show();
+                    ToCashBtn.Show();
+                    ManageProductsButton.Show();
+                    found = true;
+                    break;
+                }
+
+                if (user.Email == LoginEmail_Box.Text && user.Password == LoginPassword_Box.Text && user.SecurityLevel == "Employee")
+                {
+                    LoginPanel.Hide();
+                    found = true;
+                    Hide();
+                    Cashregister CashregisterForm = new Cashregister();
+                    CashregisterForm.Show();
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                LoginPassword_Box.Clear();
+                IncorrectLabel.Show();
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.ClientSize = new System.Drawing.Size(2382, 1303);
+            this.Name = "LoginForm";
+            this.ResumeLayout(false);
+
         }
     }
 }
